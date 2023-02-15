0213c21e961353d19f78a25eda67e0221735377d
Christian Rodriguez <105233007+ChrisAlexRods@users.noreply.github.com>
2023-02-14 19:29:09 -0500

    Fixed docker Yaml

--- api/migrations/001_accounts_table.py
+++ api/migrations/001_accounts_table.py
@@ -6,7 +6,7 @@ steps = [
             id SERIAL PRIMARY KEY NOT NULL,
             name VARCHAR(100) NOT NULL,
             email VARCHAR(200) UNIQUE NOT NULL,
-            password VARCHAR(20) NOT NULL
+            hashed_password TEXT NOT NULL
         );
         """,
         # DROP TABLE
