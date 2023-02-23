1d3161e54865acd50160599ac987ce5efb78189b
Christian Rodriguez <105233007+ChrisAlexRods@users.noreply.github.com>
2023-02-23 10:13:12 -0500

    journals

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,18 +1,18 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider, useToken } from './Token';
-import './App.css';
-import Nav from './Nav';
-import MainPage from './MainPage';
-import LoginPage from './LoginPage'
-import SignUp from './SignUp'
-
+import ErrorNotification from "./ErrorNotification";
+import { AuthProvider, useToken } from "./Token";
+import "./App.css";
+import Nav from "./Nav";
+import MainPage from "./MainPage";
+import LoginPage from "./LoginPage";
+import SignUp from "./SignUp";
 
 function GetToken() {
   // Get token from JWT cookie (if already logged in)
   useToken();
   return null;
 }
-
 
 function App() {
   return (
