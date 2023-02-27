435b3510a4baa946aaa8e21159c1ca67b68137ec
Megan <megann1006@yahoo.com>
2023-02-27 10:48:43 -0600

    added img:

--- ghi/src/HomePage.js
+++ ghi/src/HomePage.js
@@ -1,35 +1,62 @@
-import { useToken } from './Token';
+import { useToken, getTokenInternal } from './Token';
 import { useState, useEffect } from 'react';
-import { Carousel } from 'react-bootstrap';
+import { useNavigate, Link } from "react-router-dom";
 
 
 function HomePage() {
 
-  const { token } = useToken()
-  const [testClass, setTestClass] = useState()
+  const [plants, setPlants] = useState([]);
+  const { token } = useToken();
+  const navigate = useNavigate();
 
-  const isLoggedIn = () => {
+
+  // const getPlants = async () => {
+  //   const token = await getTokenInternal();
+  //   const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/plants/category/Fern/`;
+  //   try {
+  //     const response = await fetch(url, {
+  //       method: 'get',
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //       credentials: 'include'
+  //     });
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       setPlants(data);
+  //     }
+  //   } catch (error) {
+  //   }
+  // }
+
+
+  const isLoggedIn = async () => {
+    const token = await getTokenInternal()
     if (!token) {
-      setTestClass("display-5 fw-bold d-none")
-    } else {
-      setTestClass("display-5 fw-bold")
+      setTimeout(() => {
+        navigate("/login");
+      }, 0);
     }
   }
 
+
   useEffect(() => {
+    // getPlants();
     isLoggedIn();
-  });
+  }, [token]);
 
 
   return (
     <div className="px-4 py-5 my-5 text-center">
-      <h1 className={testClass}>Green Thumb</h1>
+      <h1>Green Thumb</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
           house plant care website
         </p>
       </div>
+      {/* {plants.map((plant) => ( */}
       <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
+          {/* <Link to={`/plants/${plant.api_id}`}> */}
         <div className="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -37,13 +64,13 @@ function HomePage() {
         </div>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src="..." className="d-block w-100" alt="..." />
+                <img src="http://www.tropicopia.com/house-plant/thumbnails/5556.jpg" className="w-25" alt="" />
           </div>
           <div className="carousel-item">
-            <img src="..." className="d-block w-100" alt="..." />
+              <img src="http://www.tropicopia.com/house-plant/thumbnails/5725.jpg" className="w-25" alt="..." />
           </div>
           <div className="carousel-item">
-            <img src="..." className="d-block w-100" alt="..." />
+              <img src="http://www.tropicopia.com/house-plant/thumbnails/5491.jpg" className="w-25" alt="..." />
           </div>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -54,7 +81,9 @@ function HomePage() {
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Next</span>
         </button>
+          {/* </Link> */}
       </div>
+    {/* ))} */}
     </div>
   );
 }
