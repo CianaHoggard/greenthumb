e694accff5277847414365216ab679dc4f5a283e
Matthew <mattsun220@gmail.com>
2023-03-06 17:07:10 -0500

    Homepage carousel dynamic

--- ghi/src/HomePage.js
+++ ghi/src/HomePage.js
@@ -101,7 +101,7 @@ function HomePage() {
         <div>
           <h3 className='space'>Staff Picks</h3>
         </div>
-        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
+        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
           <div className="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
