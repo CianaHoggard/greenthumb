73e7c97961cc811365bef18fb858b9eb1146c5f8
MeganR106 <106038555+MeganR106@users.noreply.github.com>
2023-03-09 10:51:53 -0600

    started deployment

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -20,9 +20,11 @@ function GetToken() {
 }
 
 function App() {
+  const domain = /https:\/\/[^/]+/;
+  const basename = process.env.PUBLIC_URL.replace(domain, '');
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <AuthProvider>
         <Nav />
         <GetToken />
